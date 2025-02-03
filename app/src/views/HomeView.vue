a76a75b33a4a14f60451db51714112a79f073a3f
WilsonD09 <wilsond34@nycstudents.net>
2025-02-03 12:30:36 -0500

    s

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <!-- <TheWelcome /> -->
-  </div>
+  <div></div>
 </template>
